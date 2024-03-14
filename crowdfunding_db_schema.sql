6d88ea8632f8b43222ed1b008114e20058875e39
tokarczykbl <tokarczykbl@users.noreply.github.com>
2024-03-14 19:56:37 -0400

    Saving updated schema file

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,62 +1,48 @@
-CREATE TABLE "campaign" (
-    "cf_id" int   NOT NULL,
-    "contact_id" int   NOT NULL,
-    "company_name" VARCHAR(100)   NOT NULL,
-    "description" VARCHAR(100)   NOT NULL,
-    "goal" NUMERIC(10,2)   NOT NULL,
-    "pledged" NUMERIC(10,2)   NOT NULL,
-    "outcome" VARCHAR(20)   NOT NULL,
-    "backers_count" int   NOT NULL,
-    "country" VARCHAR(20)   NOT NULL,
-    "currency" VARCHAR(20)   NOT NULL,
-    "launched_date" DATE   NOT NULL,
-    "end_date" DATE   NOT NULL,
-    "category_id" VARCHAR(20)   NOT NULL,
-    "subcategory_id" VARCHAR(20)   NOT NULL,
-    CONSTRAINT "pk_campaign" PRIMARY KEY (
-        "cf_id"
-     )
+CREATE TABLE contacts (
+    contact_id int   NOT NULL,
+    first_name VARCHAR(50)   NOT NULL,
+    last_name VARCHAR(50)   NOT NULL,
+    email VARCHAR(50)   NOT NULL,
+    PRIMARY KEY (contact_id)
 );
 
-CREATE TABLE "category" (
-    "category_id" VARCHAR(10)   NOT NULL,
-    "category" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_category" PRIMARY KEY (
-        "category_id"
-     )
+CREATE TABLE category (
+    category_id VARCHAR(10)   NOT NULL,
+    category VARCHAR(50)   NOT NULL,
+    PRIMARY KEY (category_id)
 );
 
-CREATE TABLE "contacts" (
-    "contact_id" int   NOT NULL,
-    "first_name" VARCHAR(50)   NOT NULL,
-    "last_name" VARCHAR(50)   NOT NULL,
-    "email" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_contacts" PRIMARY KEY (
-        "contact_id"
-     )
+CREATE TABLE subcategory (
+    subcategory_id VARCHAR(20)   NOT NULL,
+    subcategory VARCHAR(50)   NOT NULL,
+    PRIMARY KEY (subcategory_id)
 );
 
-CREATE TABLE "subcategory" (
-    "subcategory_id" VARCHAR(20)   NOT NULL,
-    "subcategory" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_subcategory" PRIMARY KEY (
-        "subcategory_id"
-     )
+CREATE TABLE campaign (
+    cf_id int   NOT NULL,
+    contact_id int   NOT NULL,
+    company_name VARCHAR(100)   NOT NULL,
+    description VARCHAR(100)   NOT NULL,
+    goal NUMERIC(10,2)   NOT NULL,
+    pledged NUMERIC(10,2)   NOT NULL,
+    outcome VARCHAR(20)   NOT NULL,
+    backers_count int   NOT NULL,
+    country VARCHAR(20)   NOT NULL,
+    currency VARCHAR(20)   NOT NULL,
+    launched_date DATE   NOT NULL,
+    end_date DATE   NOT NULL,
+    category_id VARCHAR(20)   NOT NULL,
+    subcategory_id VARCHAR(20)   NOT NULL,
+    PRIMARY KEY (cf_id),
+	FOREIGN KEY (contact_id) REFERENCES contacts (contact_id),
+	FOREIGN KEY (category_id) REFERENCES category (category_id),
+	FOREIGN KEY (subcategory_id) REFERENCES subcategory (subcategory_id)
 );
-
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
-REFERENCES "contacts" ("contact_id");
-
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
-REFERENCES "subcategory" ("subcategory_id");
-
-ALTER TABLE "category" ADD CONSTRAINT "fk_category_category_id" FOREIGN KEY("category_id")
-REFERENCES "campaign" ("category_id");
 
 SELECT * FROM campaign;
 
 SELECT * FROM category;
 
-SELECT * FROM contacts;
-
 SELECT * FROM subcategory;
+
+SELECT * FROM contacts;
